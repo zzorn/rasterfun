ddb35f96847576762e89ae15c22e56f5f17a3fdf
zzorn <zzorn@iki.fi>
2011-04-11 17:21:15 +0300

    Added IntelliJ IDEA project file generation plugin, to allow generating project files for IDEA if desired.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,10 +1,12 @@
 
 import sbt._
 
-class Project(info: ProjectInfo) extends DefaultProject(info)
+class Project(info: ProjectInfo) extends DefaultProject(info) with IdeaProject
 {
   // Scala unit testing
   val scalatest = "org.scalatest" % "scalatest" % "1.3"
+
+  val janino = "org.codehaus.janino" % "janino" % "2.5.16"
 
 //  def fooJars = descendents("lib" / "foo", "*.jar")
 //  override def unmanagedClasspath = super.unmanagedClasspath +++ fooJars
