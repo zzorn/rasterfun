863190cc4da1e4edca57c3b47b9c586281bb9384
zzorn <zzorn@iki.fi>
2011-06-16 12:16:57 +0300

    A UI component visualizing a gradient.

---
+++ src/main/scala/org/rasterfun/util/gradient/GradientView.scala
@@ -0,0 +1,62 @@
+package org.rasterfun.util.gradient
+
+import javax.swing.{JPanel, JComponent}
+import simplex3d.math.float._
+import simplex3d.math.float.functions._
+import java.awt.{Color, Graphics2D, Graphics}
+
+/**
+ * An UI component that shows a gradient.
+ */
+class GradientView(var gradient: Gradient) extends JPanel {
+
+  var alphaGridSize = 16
+  var alphaGrey1 = 0.6f
+  var alphaGrey2 = 0.4f
+
+  var start = 0f
+  var end = 1f
+
+  private def makeColor(relativePos: Float, greyLevel: Float): Color = {
+    val g = gradient.apply(relativePos)
+    new Color(
+      mix(greyLevel, g.r, g.a),
+      mix(greyLevel, g.g, g.a),
+      mix(greyLevel, g.b, g.a)
+    )
+  }
+
+  override def paintComponent(g: Graphics) {
+    val g2 = g.asInstanceOf[Graphics2D]
+
+    val w = getWidth
+    val h = getHeight
+
+    var x = 0
+    while (x < w) {
+
+      val relativePos = start + (1.0f * x / w) * (end - start)
+      val c1 = makeColor(relativePos, alphaGrey1)
+      val c2 = makeColor(relativePos, alphaGrey2)
+
+      var y = 0
+      var gridSelect = ((x % alphaGridSize) % 2) == 0
+      while (y < h) {
+        val y2 = min(h, y + alphaGridSize - 1)
+
+        if (y2 > y) {
+          if (gridSelect) g2.setColor(c1)
+          else g2.setColor(c2)
+
+          g2.drawLine(x, y, x, y2)
+        }
+
+        y += alphaGridSize
+        gridSelect = !gridSelect
+      }
+
+      x += 1
+    }
+  }
+
+}
