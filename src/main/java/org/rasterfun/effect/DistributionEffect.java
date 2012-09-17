fd5af7051a97eb4cee6e5e6932d3fd61e3d416e6
zzorn <zzorn@iki.fi>
2012-09-17 16:10:42 +0300

    Started sketching out distributor.

---
+++ src/main/java/org/rasterfun/effect/DistributionEffect.java
@@ -0,0 +1,9 @@
+package org.rasterfun.effect;
+
+/**
+ * Distributes pictures over an area
+ */
+public class DistributionEffect extends EffectBase {
+
+
+}
---
+++ src/main/java/org/rasterfun/utils/Distributor.java
@@ -0,0 +1,12 @@
+package org.rasterfun.utils;
+
+/**
+ * Point distribution over space / over an area.
+ */
+public interface Distributor {
+
+    void
+
+
+
+}
